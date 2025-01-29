ee26461ac0628e3df79974a85b6bc5c2e66c3fc5
Stathis Kolovos <stathiskolovos@MacBook.local>
2025-01-29 16:13:07 +0200

    bug fix

--- src/Components/EventPlanner.jsx
+++ src/Components/EventPlanner.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './EventPlanner.css'; // Import CSS file for styling
+import Footer from './Footer';
 
 const EventPlanner = () => {
     return (
@@ -73,6 +74,7 @@ const EventPlanner = () => {
                     <button className="submit-button">Send</button>
                 </form>
             </section>
+            <Footer/>
         </div>
     );
 };
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './EventPlanner.css';
+
+const EventPlannerFooter = () => {
+    return (
+       <footer>
+        <p>&copy; Event Planner Organization. All rights reserved</p>
+       </footer>
+    )
+};
+
+export default EventPlannerFooter;
